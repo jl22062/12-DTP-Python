e0028b5839e23380610e5dd458df77bb3eaea7ba
BotSolo621 <201015004+jl22062@users.noreply.github.com>
2025-05-21 08:52:45 +1200

    Create character.py

---
+++ Lists/character.py
@@ -0,0 +1,8 @@
+soldier = {
+    "legalInfo":{"title":"Mister", "firstName":"Jane", "lastName":"Doe","age":"50","gender":"male"},
+    "PrimearyWeapon":"Rocket Launcher",
+    "SecondaryWeapon":"Shotgun",
+    "Melee":"Shovel",
+    "Position":"Shcok and Awe"
+}
+
